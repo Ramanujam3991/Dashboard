415c517b75b7982252d64ae840e8febc2d9bb51c
ramanujam3991 <lavanyavaidehi@gmail.com>
2026-04-08 09:10:39 -0400

    T0.4: scaffold main package with Electron entry

---
+++ packages/main/src/index.ts
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // We will load the Vite dev server URL or file in later tasks,
+  // for now we just load a blank page.
+  win.loadURL('about:blank');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
